84213d9a45b39216d5a39a79d3bc1bfcf6f45ab1
Steven Haryanto <stevenharyanto@gmail.com>
2012-11-16 11:52:20 +0000

    Import of SHARYANTO/Data-Format-Pretty-YAML-0.03 from CPAN.
    gitpan-cpan-distribution: Data-Format-Pretty-YAML
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SHARYANTO/Data-Format-Pretty-YAML-0.03.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Data/Format/Pretty/YAML.pm
+++ lib/Data/Format/Pretty/YAML.pm
@@ -4,13 +4,11 @@ use 5.010;
 use strict;
 use warnings;
 
-use YAML::Syck;
-
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(format_pretty);
 
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 
 sub content_type { "text/yaml" }
 
@@ -18,10 +16,17 @@ sub format_pretty {
     my ($data, $opts) = @_;
     $opts //= {};
 
-    local $YAML::Syck::ImplicitTyping = 1;
-    local $YAML::Syck::SortKeys       = 1;
-    local $YAML::Syck::Headless       = 1;
-    Dump($data);
+    if ($opts->{color} // (-t STDOUT)) {
+        require YAML::Tiny::Color;
+        local $YAML::Tiny::Color::LineNumber = 1;
+        YAML::Tiny::Color::Dump($data);
+    } else {
+        require YAML::Syck;
+        local $YAML::Syck::ImplicitTyping = 1;
+        local $YAML::Syck::SortKeys       = 1;
+        local $YAML::Syck::Headless       = 1;
+        YAML::Syck::Dump($data);
+    }
 }
 
 1;
@@ -36,7 +41,7 @@ Data::Format::Pretty::YAML - Pretty-print data structure as YAML
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 SYNOPSIS
 
@@ -68,6 +73,21 @@ YAML::Syck's settings are optimized for prettiness, currently as follows:
  $YAML::Syck::ImplicitTyping = 1;
  $YAML::Syck::SortKeys       = 1;
  $YAML::Syck::Headless       = 1;
+
+Options:
+
+=over
+
+=item * color => BOOL
+
+Whether to enable coloring. The default is the enable only when running
+interactively. Currently also enable line numbering.
+
+=back
+
+=head2 content_type() => STR
+
+Return C<text/yaml>.
 
 =head1 SEE ALSO
 
